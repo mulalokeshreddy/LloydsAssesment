374ca848d9fc89c067c7cd2d0306474cb407a9a4
LOKESH <LOKESH@DESKTOP-PL9EER3>
2019-01-07 12:06:31 +0530

    commit

---
+++ LloydsAssesment-2/src/main/java/com/assesment/LloydsAssesment2Application.java
@@ -0,0 +1,14 @@
+package com.assesment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LloydsAssesment2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LloydsAssesment2Application.class, args);
+	}
+
+}
+
