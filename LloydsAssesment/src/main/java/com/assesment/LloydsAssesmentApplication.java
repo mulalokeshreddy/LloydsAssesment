eea61bd3c9b6d899e01e4c93c03c8aefa4806ec9
LOKESH <LOKESH@DESKTOP-PL9EER3>
2019-01-07 12:01:00 +0530

    commit

---
+++ LloydsAssesment/src/main/java/com/assesment/LloydsAssesmentApplication.java
@@ -0,0 +1,14 @@
+package com.assesment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LloydsAssesmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LloydsAssesmentApplication.class, args);
+	}
+
+}
+
---
+++ LloydsAssesment/src/test/java/com/assesment/LloydsAssesmentApplicationTests.java
@@ -0,0 +1,17 @@
+package com.assesment;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LloydsAssesmentApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
